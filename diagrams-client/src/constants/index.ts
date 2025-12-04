90f0a622779a358522cb6ae1c7f9e0047b5fa88f
Naik, Harsh <v-hnaik@mirion.com>
2025-12-04 13:45:40 +0530

    refactor GitHub API constants

---
+++ diagrams-client/src/constants/index.ts
@@ -0,0 +1,10 @@
+export const GITHUB_API_BASE =
+  "https://api.github.com/repos/harsh07may/Neetcode-150/contents";
+export const IGNORE_DIRECTORIES = [
+  "diagrams-client",
+  ".github",
+  ".git",
+  ".vscode",
+];
+export const GITHUB_REPOSITORY_URL =
+  "https://raw.githubusercontent.com/harsh07may/Neetcode-150/main";
